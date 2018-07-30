22d498366d50687896ffb98ba4108dd45888f559
xaralis <filip.varecha@fragaria.cz>
2018-07-30 16:03:06 +0200

    [feature] working service worker implementation

--- assets/js/register-sw.js
+++ assets/js/register-sw.js
@@ -3,44 +3,9 @@ layout: null
 ---
 
 if ("serviceWorker" in navigator) {
-    function listenForWaitingServiceWorker(reg, callback) {
-        function awaitStateChange() {
-            reg.installing.addEventListener('statechange', function() {
-                if (this.state === 'installed') callback(reg);
-            });
-        }
-        if (! reg) return;
-        if (reg.waiting) return callback(reg);
-        if (reg.installing) awaitStateChange();
-
-        reg.addEventListener('updatefound', awaitStateChange);
-    }
-
-    function promptUserToRefresh(reg) {
-        // this is just an example
-        // don't use window.confirm in real life; it's terrible
-        if (window.confirm("New version available! OK to refresh?")) {
-            // send message to service workers to reload themselves.
-            reg.waiting.postMessage('skipWaiting');
-        }
-    }
-
     navigator.serviceWorker.register("{{ site.baseurl }}sw.js").then(function(registration) {
         console.log("[sw] Service Worker registration successful with scope: ", registration.scope);
-        listenForWaitingServiceWorker(registration, promptUserToRefresh);
     }).catch(function(err) {
         console.log("[sw] Service Worker registration failed: ", err);
-    });
-
-
-    // reload once when the new Service Worker starts activating
-    var refreshing;
-    navigator.serviceWorker.addEventListener('controllerchange', function() {
-        if (refreshing) {
-            return;
-        }
-
-        refreshing = true;
-        window.location.reload();
     });
 }
--- sw.js
+++ sw.js
@@ -4,74 +4,84 @@ layout: null
 const urls = [];
 
 // Cache assets
-{%- for asset in site.static_files -%}
+{% for asset in site.static_files -%}
 {%- if asset.path contains '/assets/fonts' or asset.path contains '/assets/icons' or asset.path contains '/assets/img' or asset.extname == '.js' -%}
 urls.push("{{ asset.path | relative_url }}");
 {%- endif -%}
-{%- endfor -%}
-
+{%- endfor %}
 
 // Cache posts
-{%- for post in site.posts limit: 10 -%}
+{% for post in site.posts limit: 10 -%}
 urls.push("{{ post.url | relative_url }}");
-{%- endfor -%}
+{%- endfor %}
 
 // Cache pages
-{%- for page in site.html_pages -%}
+{% for page in site.html_pages -%}
 {%- unless page.path contains 'amp-html' or page.path contains 'blog/archive' -%}
 urls.push("{{ page.url | relative_url }}");
 {%- endunless -%}
-{%- endfor -%}
+{%- endfor %}
 
 
 // Cache name: adjust version number to invalidate service worker cache.
-const CACHE_NAME = 'fragariacz-cache-v1';
+const CACHE_NAME = 'fragariacz-cache-v{{ site.service_worker.cache_version }}';
+
+function precache() {
+    caches.open(CACHE_NAME).then(function(cache) {
+        console.log(`[sw] Opened cache ${CACHE_NAME}`);
+        return cache.addAll(urls);
+    });
+}
+
+function storeInCache(request, response) {
+    caches.open(CACHE_NAME).then(cache => {
+        cache.put(request, response);
+    });
+}
+
+function loadFromNetwork(request, timeout) {
+    return new Promise((fulfill, reject) => {
+        let tid = setTimeout(reject, timeout);
+
+        fetch(request).then(response => {
+            clearTimeout(tid);
+            const responseClone = response.clone();
+            fulfill(response);
+            storeInCache(request, responseClone);
+        }, reject);
+    });
+}
+
+function loadFromCache(request) {
+    return caches.open(CACHE_NAME).then(cache => {
+        return cache.match(request).then(matching => {
+            return matching || Promise.reject('no-match');
+        });
+    });
+}
 
 self.addEventListener('install', function(event) {
     // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            console.log(`[sw] Opened cache ${CACHE_NAME}`);
-            return cache.addAll(urls);
-        })
-    );
+    event.waitUntil(precache());
 });
 
-addEventListener('activate', activateEvent => {
-    activateEvent.waitUntil(
-        caches.keys().then(keyList => Promise.all(keyList.map(key => {
-            if (key !== CACHE_NAME) {
-                return caches.delete(key);
-            }
-        })))
-    );
-});
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    let loadPipe = loadFromNetwork(request, 1500).catch(err => loadFromCache(request));
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(resp) {
-            if (resp) {
-                return resp;
-            }
+    if (request.method === 'GET') {
+        loadPipe = loadPipe.catch(err => {
+            console.warn('[sw] Could not load, redirecting to offline page ', err);
+            return caches.open(CACHE_NAME).then(cache => {
+                return cache.match('{{ "/offline/" | relative_url }}');
+            })
+        });
+    } else {
+        loadPipe = loadPipe.catch(err => {
+            console.warn('[sw] Could not load, going for 404', err);
+            throw err;
+        });
+    }
 
-            return fetch(event.request).then(function(response) {
-                let responseClone = response.clone();
-
-                caches.open(CACHE_NAME).then(function(cache) {
-                    cache.put(event.request, responseClone);
-                });
-
-                return response;
-            });
-        }).catch(function (err) {
-            console.warn('[sw] Could not load ', err);
-            return caches.match('{{ site.baseurl }}offline/');
-        })
-    );
-});
-
-// Listen to skipWaiting message, that can be issued when user wants to update the service worker.
-// @note: message is posted from register-sw.js script.
-self.addEventListener('message', messageEvent => {
-    if (messageEvent.data === 'skipWaiting') return skipWaiting();
+    event.respondWith(loadPipe);
 });

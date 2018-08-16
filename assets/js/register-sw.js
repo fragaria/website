4217e0eea17eddcf168b86bb92d4ac37cd5b0fe3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-16 13:30:31 +0000

    Deploy fragaria/website to github.com/fragaria/website.git:gh-pages

--- assets/js/register-sw.js
+++ assets/js/register-sw.js
@@ -1,1 +1,7 @@
-if ("serviceWorker" in navigator) { navigator.serviceWorker.register("/website/sw.js").then(function(registration) { console.log("[sw] Service Worker registration successful with scope: ", registration.scope); }).catch(function(err) { console.log("[sw] Service Worker registration failed: ", err); }); }
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/website/sw.js").then(function(registration) {
+        console.log("[sw] Service Worker registration successful with scope: ", registration.scope);
+    }).catch(function(err) {
+        console.log("[sw] Service Worker registration failed: ", err);
+    });
+}

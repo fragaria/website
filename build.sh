9eb4dea3436b5508b6a217750a0f4636bf3bc16a
Martin Bilek <martin.bilek@gmail.com>
2025-08-10 13:49:15 +0200

    update build.sh script to use bundler

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail
 
-set -e
+IMG="fragaria-website"
+CFG="_config.yml,_config.witharchives.yml,_config.prod.yml"
 
-docker build . -t fragaria-website
-docker run -e JEKYLL_ENV=production -v $PWD:/srv/jekyll fragaria-website jekyll build --config '_config.yml,_config.witharchives.yml,_config.prod.yml'
+docker build . -t "$IMG"
+
+docker run --rm \
+  --user "$(id -u):$(id -g)" \
+  -e JEKYLL_ENV=production \
+  -v "$PWD":/srv/jekyll -w /srv/jekyll \
+  "$IMG" bundle exec jekyll build --config "$CFG"
+
 mkdir -p /tmp/fragaria-website-build
 cp -r _site/* /tmp/fragaria-website-build
 rm -rf _site
 git checkout gh-pages
-cp -r /tmp/fragaria-website-build/ .
-git commit -am "Automated build"
-
-
+cp -r /tmp/fragaria-website-build/. .
+# git add -A
+# git commit -m "Automated build"
